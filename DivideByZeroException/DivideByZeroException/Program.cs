8fec72f7ec3a6401144cc0272c43fc70320a0037
Inoka Sandamali <49406979+Sandamali94@users.noreply.github.com>
2020-01-13 16:13:10 +0530

    Add files via upload

---
+++ DivideByZeroException/DivideByZeroException/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DivideByException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader sr = null;
+                sr = File.OpenText(@"F:\data.txt");
+
+                Console.WriteLine(sr.ReadToEnd());
+
+                int num1 = int.Parse(Console.ReadLine());
+                int num2 = int.Parse(Console.ReadLine());
+
+                int res = num1 / num2;
+
+                Console.WriteLine(res);
+            }
+            catch (DivideByZeroException de) {    
+            Console.WriteLine("Your File is not in Correct Location");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
