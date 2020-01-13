37f1e550c284e66e39efe8ebee23c34331f53b52
Inoka Sandamali <49406979+Sandamali94@users.noreply.github.com>
2020-01-13 16:12:28 +0530

    Add files via upload

---
+++ Exception/Exception/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader sr = null;
+                sr = File.OpenText(@"F:\data.txt");
+
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            catch(FileNotFoundException fx)
+            {
+                Console.WriteLine("Your File is not in Correct Location");
+            }
+                         
+            Console.ReadLine();
+
+        }
+    }
+}
+
